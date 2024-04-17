83431e92563f3234d55cfb2da22ed3ff30bb34bd
Ali-Haider-Sherazi <syedalisherazi55@gmail.com>
2024-04-18 00:37:52 +0500

    price status cganged in  api

--- models/arenaModel.js
+++ models/arenaModel.js
@@ -57,7 +57,7 @@ const arenaSchema = new mongoose.Schema(
       required: [true, "Whats game play in the Arena"],
     },
     price: {
-      type: Number,
+      type: String,
       required: [true, "What is the hourly charge"],
     },
     reviews: {
